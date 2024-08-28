351cb061ce22d8eb03e77097e2be813083cffa3b
whrz4642 <tiff4642@gmail.com>
2024-08-28 14:52:48 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+#fridge {
+    width: 300px;
+    height: 400px;
+    background-color: #f0f0f0;
+    border: 2px solid #ccc;
+    position: relative;
+}
+
+.shelf {
+    height: 50px;
+    background-color: #ddd;
+    border-bottom: 1px solid #ccc;
+    width: 100%;
+}
+
+.item {
+    width: 80px;
+    height: 40px;
+    background-color: #ff9999;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    cursor: pointer;
+}
